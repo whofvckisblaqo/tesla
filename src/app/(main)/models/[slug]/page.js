f814ef64e8f997f393e71bf2e998dd800145f83c
kingsleyonyekwelu <kimonyekwelu@gmail.com>
2026-05-16 14:37:00 +0100

    model slug correction

--- src/app/(main)/models/[slug]/page.js
+++ src/app/(main)/models/[slug]/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect, useRef } from "use client";
+import { useState, useEffect, useRef } from "react";
 import { useParams } from "next/navigation";
 import Link from "next/link";
 import Navbar from "@/components/layout/Navbar";
